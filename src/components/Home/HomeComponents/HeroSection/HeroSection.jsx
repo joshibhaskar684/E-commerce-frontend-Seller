be596df8329cb2d9c9f393be78eb4afc5f785027
joshibhaskar684 <joshibhaskar684@gmail.com>
2026-02-28 15:28:33 +0530

    herosection improved

--- src/components/Home/HomeComponents/HeroSection/HeroSection.jsx
+++ src/components/Home/HomeComponents/HeroSection/HeroSection.jsx
@@ -2,17 +2,18 @@ export default function HeroSection() {
     return (
         <>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 p-5 lg:p-40 lg:pt-10 gap-10 bg-background/10 text-foreground  ">
-                <div className="grid grid-cols-1 py-5 gap-6 ">
-                    <h1 className="lg:text-5xl md:text-4xl text-3xl font-bold ">
-                        Sell Your Products Online in Minutes 🚀
+            <div className=" grid grid-cols-1  p-5 gap-10 ">
+                <div className=" grid grid-cols-1  place-items-center gap-10 md:p-5 lg:p-20">
+                    <h1 className="lg:text-8xl md:text-5xl text-4xl font-bold ">
+                        Sell Your Products Online in{" "}
+                        <span className="text-yellow-300">Minutes</span>
                     </h1>
                     <p className="text-lg text-gray-600 text-justify">
                         Launch your own online store with our powerful and easy-to-use platform.
                         No technical skills required. Start selling, manage orders, and grow your business — all in one place.
                     </p>
 
-                    <div className="grid grid-cols-2 gap-2">
+                    <div className="grid grid-cols-1  md:grid-cols-2 gap-2  w-full md:w-1/3">
                         <button className="border border-foreground cursor-pointer  p-4 rounded bg-yellow-500 text-background font-bold">
                             Get Started
                         </button>
@@ -27,11 +28,13 @@ export default function HeroSection() {
                         <span>✔ Secure payments</span>
                     </div>
                 </div>
-                <div className=" flex items-center justify-center">
-                    <img src="/HomeheroSection.png" alt="Hero Image" className="w-full h-auto" />
-                </div>
+                <div className=" grid grid-cols-1 place-items-center md:p-5 ">
+                    <img src="/HomeheroSection.png" alt="Hero Image" className="w-full h-auto md:h-screen border rounded border-2 " />
 
+                </div>
             </div>
+
+
         </>
     )
 }
