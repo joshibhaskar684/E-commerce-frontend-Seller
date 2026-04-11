f216ebbba267d70d0c59ef3174bafeb0baa06f47
joshibhaskar684 <joshibhaskar684@gmail.com>
2026-04-11 20:14:53 +0530

    product page created

---
+++ src/app/(seller)/seller/(seller)/products/page.jsx
@@ -0,0 +1,12 @@
+export default function page(){
+    return(
+        <>
+        <div className="grid grid-cols-1">
+            <div className="">
+                <h1 className=" text-foreground text-xl">Products </h1>
+            </div>
+
+            </div>
+        </>
+    )
+}
